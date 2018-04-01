d2dbfdc223f97fd1e203a9f59298b820c87f9936
Nadya <n.perehodko@yandex.com>
2018-04-01 22:22:31 +0300

    Add files via upload

--- calculations/script_file_squish.py
+++ calculations/script_file_squish.py
@@ -4,20 +4,25 @@
 #####################################################################
 
 def all():
-    global route_b
+    global route_b, main_b, measure_b, measure_two_point_b
     global okButton
-    global b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_0, plus_minus_b, comma_b
+    global b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8, b_9, b_0, plus_minus_b, comma_b, backspace_b, backspace_b_2
     global dict_keyboard
     global geo_calc_b
 
     #  Режим "Маршруты"
     route_b = "{name='m_btnRoutes' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"  # Кнопка Маршруты в главном меню
 
+    #  Главное меню
+    main_b = "{name='m_btnMain' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}" #Кнопка "Основной"
+    measure_b = "{occurrence='7' type='QPushButton' unnamed='1' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"  # Измерения
+    measure_two_point_b = "{occurrence='10' type='QPushButton' unnamed='1' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"  # Расстояние между двумя точками
+
     # Геодезический калькулятор (левая панель)
     geo_calc_b = "{name='qpb_side_left-route_geodesia' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"  
 
-    # Виртуальная клавиатура
-    okButton = "{name='m_qpbOK' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"  # Кнопка ОК 
+    # Виртуальная клавиатура прямоугольная
+    okButton = "{name='m_qpbOK' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"  # Кнопка ОК на виртуальной клавиатуре
     b_1 = "{name='m_qpb1' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"
     b_2 = "{name='m_qpb2' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"
     b_3 = "{name='m_qpb3' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"
@@ -28,10 +33,10 @@ def all():
     b_8 = "{name='m_qpb8' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"
     b_9 = "{name='m_qpb9' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"
     b_0 = "{name='m_qpb0' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"
-    comma_b = "{name='m_qpbSep' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"  # Кнопка "запятая"
+    comma_b = "{name='m_qpbSep' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"  # Кнопка Запятая
     backspace_b = "{name='m_qpbBack1' type='QPushButton' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"  # Backspace
 
-    #  Словарь со значениями кнопок в вирт.клавиатуре
+    #  Словарь со значениями кнопк в вирт клавиатуре
     dict_keyboard = {"1": b_1, "2": b_2, "3": b_3, "4": b_4, "5": b_5, "6": b_6, "7": b_7, "8": b_8, "9": b_9, "0": b_0,
                      ",": comma_b} 
 
@@ -57,4 +62,5 @@ def geocalc():
 
     # Чек-бокс "Результат в ДУ"
     result_in_DE = "{name='qchk_resultInDE' type='QCheckBox' visible='1' window=':mainWidget_gui::AnimatedStackedWidget'}"
-    
+
+   
