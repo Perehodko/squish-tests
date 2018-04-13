cb4e3c5797c2654bbdfc692c5410da8bb34a5c62
Nadya <n.perehodko@yandex.com>
2018-04-13 23:13:15 +0300

    Update remote_run_testSuite_RU.sh

--- remote_run_testSuite_RU.sh
+++ remote_run_testSuite_RU.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # create a dated directory name for the logfile
-LOGDIRECTORY=/home/nper/Desktop/Test_reults/results-`date +%Y-%m-%d-%H:%M:%S`
+LOGDIRECTORY=/home/Desktop/Test_reults/results-`date +%Y-%m-%d-%H:%M:%S`
 
 # execute the suite
-cd /home/nper/squish/bin
-./squishrunner --host 192.168.1.162 --testsuite /home/nper/Desktop/Squish_tests/suite_Geodesy_Ru/ --reportgen html,$LOGDIRECTORY
+cd /home/squish/bin
+./squishrunner --host 192.168.1.162 --testsuite /home/Desktop/Squish_tests/suite_Geodesy_Ru/ --reportgen html,$LOGDIRECTORY
 
 echo "
 
