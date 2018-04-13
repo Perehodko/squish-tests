ccdf930008f99580957d86639e4b8d0389af102b
Nadya <n.perehodko@yandex.com>
2018-04-13 23:06:54 +0300

    Update test_clicking.py

--- calculations/test_clicking.py
+++ calculations/test_clicking.py
@@ -11,9 +11,6 @@ y1_field = "{container=':mainWidget.qstw_mode_QStackedWidget' name='qle_ogz_y_2'
 def init():
     source(findFile("scripts", "script_file_squish.py"))
     all()
-    initial_PTS()
-    geocalc()
-    routes()
     
 point1 = 143
 point2 = 12
